616418785f2b5a3b79372da4ffedc79270b8ad2e
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2026-02-11 09:48:30 -0500

    index.html, styles.css, script.js: add basic structure to index, basic formatting to the css, specifically no margin or padding and border-box; add basic library array to script file along with book constructor and function to add to library, comment out the previous information.

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-function Books(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.info = function() {
-        return `${this.title} by ${this.author}, 
-        ${this.pages}, ${this.status}`;
-    }
+// function Books(title, author, pages, status) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.status = status;
+//     this.info = function() {
+//         return `${this.title} by ${this.author}, 
+//         ${this.pages}, ${this.status}`;
+//     }
+// }
+
+// const theHobbit = new Books('The Hobbit', 'by J.R.R Tolkien', '295 pages', 'not read yet')
+
+// console.log(theHobbit.info());
+
+const myLibrary = [];
+
+function Books() {
+ // the constructor...
 }
 
-const theHobbit = new Books('The Hobbit', 'by J.R.R Tolkien', '295 pages', 'not read yet')
-
-console.log(theHobbit.info());
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
